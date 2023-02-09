0124076528371997e44ed5b8d3750b946ea4fd50
Hasan Othman§ <michleanglo@yahoo.com>
2023-02-09 23:16:27 +0100

    get result implemented

--- BlazorAppWithChatGptIntegration/Program.cs
+++ BlazorAppWithChatGptIntegration/Program.cs
@@ -1,4 +1,4 @@
-using BlazorAppWithChatGptIntegration.Data;
+using BlazorAppWithChatGptIntegration.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -7,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-
+//builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<ReadChatGptApi>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ BlazorAppWithChatGptIntegration/Services/ReadChatGptApi.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI_API.Completions;
+using OpenAI_API;
+
+namespace BlazorAppWithChatGptIntegration.Services
+{
+    public class ReadChatGptApi
+    {
+        public string GetResult(string prompt)
+        {
+            //your OpenAI API key
+            string apiKey = "";
+            string answer = string.Empty;
+            var openai = new OpenAIAPI(apiKey);
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = prompt;
+            completion.Model = OpenAI_API.Models.Model.DavinciText;
+            // OpenAI_API.Msodel.DavinciText
+            completion.MaxTokens = 4000;
+            var result = openai.Completions.CreateCompletionAsync(completion);
+            try
+            {
+                if (result != null)
+                {
+                    foreach (var item in result.Result.Completions)
+                    {
+                        answer = item.Text;
+                    }
+                    return answer;
+                }
+                else
+                {
+                    return "sorry, try with more info";
+                }
+            }
+            catch (Exception xe)
+            {
+
+                throw;
+            }
+           
+        }
+    }
+}
